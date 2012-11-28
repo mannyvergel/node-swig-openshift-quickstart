97f0a8fd6ff0d080b8ced015e17d165d86e33449
Manny <mnvergel@gmail.com>
2012-11-28 08:38:24 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,201 @@
+#!/bin/env node
+//  OpenShift sample Node application
+var express = require('express')
+, swig = require('swig')
+  , cons = require('consolidate')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path')
+  , VIEWS_DIR = __dirname + '/views';
+var fs      = require('fs');
+
+/**
+ *  Define the sample application.
+ */
+var SampleApp = function() {
+
+    //  Scope.
+    var self = this;
+
+
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
+
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_INTERNAL_IP || '127.0.0.1';
+        global.ipaddress = self.ipaddress;
+        self.port      = process.env.OPENSHIFT_INTERNAL_PORT || 3000;
+        global.port = self.port;
+       
+    };
+
+
+    /**
+     *  Populate the cache.
+     */
+    self.populateCache = function() {
+        if (typeof self.zcache === "undefined") {
+            self.zcache = { 'index.html': '' };
+        }
+
+        //  Local cache for static content.
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    };
+
+
+    /**
+     *  Retrieve entry (content) from cache.
+     *  @param {string} key  Key identifying content to retrieve from cache.
+     */
+    self.cache_get = function(key) { return self.zcache[key]; };
+
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+     
+     
+    self.createRoutes = function() {
+        self.routeMap = { };
+
+        self.routeMap['/env'] = function(req, res) {
+            var content = 'Version: ' + process.version + '\n<br/>\n' +
+                          'Env: {<br/>\n<pre>';
+            //  Add env entries.
+            for (var k in process.env) {
+               content += '   ' + k + ': ' + process.env[k] + '\n';
+            }
+            content += '}\n</pre><br/>\n'
+            res.send(content);
+            res.send('<html>\n' +
+                     '  <head><title>Node.js Process Env</title></head>\n' +
+                     '  <body>\n<br/>\n' + content + '</body>\n</html>');
+        };
+
+        self.routeMap['/'] =routes.index;
+        
+    };
+
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express();
+        
+        //  Add handlers for the app (from the routes).
+        for (var r in routes) {
+            self.app.get(r, routes[r]);
+        }
+    };
+
+
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        //self.populateCache();
+        self.setupTerminationHandlers();
+
+        // Create the express server and routes.
+        self.initializeServer();
+        
+        var app = self.app;
+
+swig.init({
+    root: VIEWS_DIR, //Note this directory is your Views directory
+    allowErrors: true // allows errors to be thrown and caught by express
+});
+
+// assign the swig engine to .html files
+app.engine('html', cons.swig);
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', VIEWS_DIR);
+  app.set('view engine', 'html');
+  app.set('view options', { layout: false });
+  app.use(express.favicon((path.join(__dirname, '/public/images/favicon.ico'))));
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());  
+  require('swig').init({
+    root: VIEWS_DIR, //Note this directory is your Views directory
+    allowErrors: true,
+    cache: false
+});
+});
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        http.createServer(self.app).listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};   /*  Sample Application.  */
+
+
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
+
